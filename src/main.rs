62a229b5fb1f6c33ea3bfa7cacf1c0f400073878
Jamie Phillips <cphill11@gmail.com>
2019-08-25 14:32:10 -0400

    Its getting better with the basic error handling.
    More rusty

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::io;
 use std::format;
+use reqwest::Error;
 use scraper::{Html, Selector};
+
+struct Report {
+    loc: String,
+    temp: String,
+    scale: String,
+    condition: String
+}
 
 fn main() {
     print_header();
@@ -10,10 +18,21 @@ fn main() {
     let mut code = String::new();
     io::stdin().read_line(&mut code).expect("Cannot read the input.");
     
-    let html = get_html_from_web(&code);
-    let report = get_weather_from_html(&html);
+    let weather = match process_zipcode(&code) {
+        Ok(report) => report,
+        _ => String::from("An error occured while processing.")
+    };
 
-    println!("The temp in {} is {} {} and {}.", report.0, report.1, report.2, report.3)
+    println!("{}", weather);
+}
+
+fn process_zipcode(input: &str) -> Result<String, Error> {
+    let html = get_html_from_web(&input)?;
+    let formatted = match  get_weather_from_html(&html) {
+        Some(report) => format!("The temp in {} is {} {} and {}.", report.loc, report.temp, report.scale, report.condition),
+        None => String::from("An error occured while parsing the weather report.")
+    };
+    Ok(formatted)
 }
 
 fn print_header() {
@@ -22,28 +41,23 @@ fn print_header() {
     println!("---------------------------------");
 }
 
-fn get_html_from_web(zipcode: &str) -> String {
+fn get_html_from_web(zipcode: &str) -> Result<String, Error> {
     let url = format!("http://www.wunderground.com/weather-forecast/{}", zipcode);
-    let mut resp = reqwest::get(&url).unwrap();
-    resp.text().unwrap()
+    let mut resp = reqwest::get(&url)?;
+    resp.text()
 }
 
-fn get_weather_from_html(html: &str) -> (String, String, String, String) {
+fn get_weather_from_html(html: &str) -> Option<Report> {
     let document = Html::parse_document(html);
     let loc_selector = Selector::parse(".city-header > h1:nth-child(2) > span:nth-child(1)").unwrap();
     let condition_selector = Selector::parse(".condition-icon > p:nth-child(2)").unwrap();
     let temp_selector = Selector::parse(".current-temp > lib-display-unit:nth-child(1) > span:nth-child(1) > span:nth-child(1)").unwrap();
     let scale_selector = Selector::parse(".current-temp > lib-display-unit:nth-child(1) > span:nth-child(1) > span:nth-child(2) > span:nth-child(1)").unwrap();
 
-    let loc = document.select(&loc_selector).last().unwrap().inner_html();
-    let condition = document.select(&condition_selector).last().unwrap().inner_html();
-    let temp = document.select(&temp_selector).last().unwrap().inner_html();
-    let scale = document.select(&scale_selector).last().unwrap().inner_html();
-    
-    println!("{}", loc);
-    println!("{}", condition);
-    println!("{}", temp);
-    println!("{}", scale);
+    let loc = document.select(&loc_selector).last()?.inner_html();
+    let condition = document.select(&condition_selector).last()?.inner_html();
+    let temp = document.select(&temp_selector).last()?.inner_html();
+    let scale = document.select(&scale_selector).last()?.inner_html();
 
-    (loc, condition, temp, scale)
+    Some(Report { loc: loc, temp: temp, scale: scale, condition: condition})
 } 
